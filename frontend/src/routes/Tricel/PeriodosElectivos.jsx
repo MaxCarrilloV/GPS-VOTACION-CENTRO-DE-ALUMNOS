5366fad3fd58186e37319d66ed429403875e4a68
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2024-07-06 20:43:02 -0400

    avances

--- frontend/src/routes/Tricel/PeriodosElectivos.jsx
+++ frontend/src/routes/Tricel/PeriodosElectivos.jsx
@@ -1,5 +1,3 @@
-// src/routes/Tricel/PeriodosElectivos.jsx
-
 import React, { useState, useEffect } from "react";
 import {
   getPeriodos,
@@ -15,106 +13,49 @@ import {
   TableRow,
   Paper,
   Button,
-  Modal,
-  Box,
-  Typography,
-  TextField,
-  FormHelperText,
 } from "@mui/material";
 import toast, { Toaster } from "react-hot-toast";
 import PeriodoForm from "../../components/PeriodoForm";
+import DeletePeriodoModal from "../../components/DeletePeriodoModal";
 
 const PeriodosElectivos = () => {
-  const [periodos, setPeriodos] = useState([]); // Inicializar como un arreglo vacío
+  const [periodos, setPeriodos] = useState([]);
   const [openPeriodoModal, setOpenPeriodoModal] = useState(false);
-  const [fechaInicio, setFechaInicio] = useState("");
   const [selectedPeriodo, setSelectedPeriodo] = useState(null);
-  const [modalMessage, setModalMessage] = useState("");
-  const [procesoId, setProcesoId] = useState("6688c20018ec9dd4d69f0b52"); // Example procesoId
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchPeriodos = async () => {
       const data = await getPeriodos();
       console.log("Fetched periodos: ", data); // Debugging
-      setPeriodos(data || []); // Asegurar que `data` es un arreglo
+      setPeriodos(data || []);
     };
     fetchPeriodos();
   }, []);
 
   const handleClosePeriodoModal = () => {
     setOpenPeriodoModal(false);
-    setSelectedPeriodo(null); // Limpiar el periodo seleccionado al cerrar el modal
-    setFechaInicio(""); // Limpiar la fecha de inicio al cerrar el modal
+    setSelectedPeriodo(null);
   };
 
-  const handleEditPeriodo = (periodo) => {
+  const handleOpenDeleteModal = (periodo) => {
     setSelectedPeriodo(periodo);
-    setFechaInicio(periodo.fechaInicio.split("T")[0]); // Cortar la fecha para el input
+    setDeleteModalOpen(true);
+    setOpenPeriodoModal(false);
+  };
+
+  const handleOpenCreatePeriodoModal = () => {
+    setSelectedPeriodo(null);
     setOpenPeriodoModal(true);
   };
 
-  const handleChangeFechaInicio = (e) => {
-    setFechaInicio(e.target.value);
-  };
-
-  const handleSubmitPeriodo = async () => {
-    try {
-      if (!selectedPeriodo || !selectedPeriodo._id) {
-        toast.error("No se ha seleccionado un periodo válido para actualizar.");
-        return;
-      }
-
-      const formattedPeriodo = {
-        fechaInicio: fechaInicio,
-      };
-
-      const response = await updatePeriodo(
-        selectedPeriodo._id,
-        formattedPeriodo
-      );
-
-      if (response.status && response.status !== 200) {
-        setModalMessage(response.data.message);
-        toast.error(response.data.message);
-      } else {
-        const data = await getPeriodos();
-        setPeriodos(data || []);
-        setModalMessage("");
-        handleClosePeriodoModal();
-        toast.success("Periodo actualizado exitosamente!");
-      }
-    } catch (error) {
-      toast.error("Ocurrió un error al actualizar el periodo.");
-    }
-  };
-
-  const handlePeriodoCreated = async () => {
-    const data = await getPeriodos();
-    setPeriodos(data || []);
-  };
-
   const formatDate = (dateString) => {
-    const [year, month, day] = dateString.split("T")[0].split("-");
+    const date = new Date(dateString);
+    date.setDate(date.getDate() + 1); // Añade un día a la fecha
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
     return `${day}-${month}-${year}`;
-  };
-
-  const handleDeletePeriodo = async () => {
-    try {
-      if (!selectedPeriodo || !selectedPeriodo._id) {
-        toast.error("No se ha seleccionado un periodo válido para eliminar.");
-        return;
-      }
-
-      await deletePeriodo(selectedPeriodo._id);
-      const data = await getPeriodos();
-      setPeriodos(data || []);
-      setDeleteModalOpen(false);
-      setSelectedPeriodo(null);
-      toast.success("Periodo eliminado exitosamente!");
-    } catch (error) {
-      toast.error("Ocurrió un error al eliminar el periodo.");
-    }
   };
 
   return (
@@ -124,7 +65,7 @@ const PeriodosElectivos = () => {
       <Button
         variant="contained"
         color="primary"
-        onClick={() => setOpenPeriodoModal(true)}
+        onClick={handleOpenCreatePeriodoModal}
       >
         Crear Nuevo Periodo
       </Button>
@@ -148,10 +89,10 @@ const PeriodosElectivos = () => {
                   <TableCell>{periodo.nombre_etapa}</TableCell>
                   <TableCell align="center">{periodo.numero_etapa}</TableCell>
                   <TableCell align="center">
-                    {new Date(periodo.fechaInicio).toLocaleDateString()}
+                    {formatDate(periodo.fechaInicio)}
                   </TableCell>
                   <TableCell align="center">
-                    {new Date(periodo.fechaFin).toLocaleDateString()}
+                    {formatDate(periodo.fechaFin)}
                   </TableCell>
                   <TableCell align="center">{periodo.duracion}</TableCell>
                   <TableCell align="center">
@@ -168,10 +109,7 @@ const PeriodosElectivos = () => {
                         variant="contained"
                         color="error"
                         style={{ margin: "0 10px" }}
-                        onClick={() => {
-                          setSelectedPeriodo(periodo);
-                          setDeleteModalOpen(true);
-                        }}
+                        onClick={() => handleOpenDeleteModal(periodo)}
                       >
                         Eliminar
                       </Button>
@@ -187,90 +125,22 @@ const PeriodosElectivos = () => {
       <PeriodoForm
         open={openPeriodoModal}
         onClose={handleClosePeriodoModal}
-        onPeriodoCreated={handlePeriodoCreated}
-        procesoId={procesoId}
+        fetchPeriodos={async () => {
+          const data = await getPeriodos();
+          setPeriodos(data || []);
+        }}
       />
 
-      <Modal open={deleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <Typography variant="h6" component="h2">
-            Confirmar Eliminación
-          </Typography>
-          {selectedPeriodo && (
-            <Typography variant="subtitle1" component="div" sx={{ mb: 2 }}>
-              ¿Está seguro de que desea eliminar el periodo{" "}
-              <strong>{selectedPeriodo.nombre_etapa}</strong>?
-            </Typography>
-          )}
-          <Button
-            variant="contained"
-            color="error"
-            onClick={handleDeletePeriodo}
-            style={{ marginRight: "10px" }}
-          >
-            Eliminar
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setDeleteModalOpen(false)}
-          >
-            Cancelar
-          </Button>
-        </Box>
-      </Modal>
-
-      <Modal open={Boolean(selectedPeriodo)} onClose={handleClosePeriodoModal}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <Typography variant="h6" component="h2">
-            Editar Periodo
-          </Typography>
-          <TextField
-            label="Fecha de Inicio (yyyy-mm-dd)"
-            name="fechaInicio"
-            value={fechaInicio}
-            onChange={handleChangeFechaInicio}
-            fullWidth
-            margin="normal"
-            placeholder="yyyy-mm-dd"
-          />
-          {modalMessage && (
-            <FormHelperText error>{modalMessage}</FormHelperText>
-          )}
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmitPeriodo}
-            style={{ marginTop: "20px" }}
-          >
-            Actualizar
-          </Button>
-        </Box>
-      </Modal>
+      <DeletePeriodoModal
+        open={deleteModalOpen}
+        onClose={() => setDeleteModalOpen(false)}
+        selectedPeriodo={selectedPeriodo}
+        deletePeriodo={deletePeriodo}
+        refreshPeriodos={async () => {
+          const data = await getPeriodos();
+          setPeriodos(data || []);
+        }}
+      />
     </div>
   );
 };
